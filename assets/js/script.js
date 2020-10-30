3271236fd2bb5f896d2f2564fd839e89253faeb2
Adam Barron <adambarron@me.com>
2020-10-30 00:22:22 -0700

    added mapboxUrl variable that can be used once we have submit button and click listener to submit zipcode

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var zipCodeEl = document.getElementById("zipcode");
 
 function getZipCoordinates() {
     var userZip = zipCodeEl.value.trim();
+    var mapboxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + userZip + ".json?access_token=pk.eyJ1IjoiYWRhbWJhcnJvbiIsImEiOiJja2d2dm84aW4wMXA0MzBsODltNjZ5ZzFiIn0.W7Kpov0CjgFZQWXRaFlKzg"
 
     fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/94043.json?access_token=pk.eyJ1IjoiYWRhbWJhcnJvbiIsImEiOiJja2d2dm84aW4wMXA0MzBsODltNjZ5ZzFiIn0.W7Kpov0CjgFZQWXRaFlKzg").then(function(response) {
         if (response.ok) {
