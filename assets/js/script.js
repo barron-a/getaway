1b318cf46192ad666a37f0972b93085901fff6bd
Adam Barron <adambarron@me.com>
2020-10-30 00:18:06 -0700

    added function to get mapbox data based on zip code (currently only hardcoded zip) and log data response

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
+var zipCodeEl = document.getElementById("zipcode");
+
 function getZipCoordinates() {
+    var userZip = zipCodeEl.value.trim();
+
     fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/94043.json?access_token=pk.eyJ1IjoiYWRhbWJhcnJvbiIsImEiOiJja2d2dm84aW4wMXA0MzBsODltNjZ5ZzFiIn0.W7Kpov0CjgFZQWXRaFlKzg").then(function(response) {
-        console.log(response);
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data);
             });
+        } else {
+
         };
     })
 }
