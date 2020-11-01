51f483d999449d35b5c05341bf81ce4d3cadd6dc
Adam Barron <adambarron@me.com>
2020-11-01 10:37:29 -0800

    adjust more references to get distance dropdown working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 var zipCodeEl = document.getElementById("zipcode");
 
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('select');
+//     var instances = M.FormSelect.init(elems, options);
+// });
+
+//note we are going to need this !advanced note: When dynamically changing the value of a textarea with methods like jQuery's
+// .val(), you must trigger an autoresize on it afterwords because .val() does not automatically trigger 
+// the events we've binded to the textarea.
+//$('#textarea1').val('New Text');
+//M.textareaAutoResize($('#textarea1'));
+
+// Or with jQuery 
+
+$(document).ready(function () {
+    $('select').formSelect();
+});
+
 function getZipCoordinates() {
     // will pull user zip from input field - likely need a search or submit button with listener
     var userZip = zipCodeEl.value.trim();
@@ -27,20 +44,3 @@ function getZipCoordinates() {
 
 getZipCoordinates();
 
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-});
-
-// Or with jQuery 
-
-$(document).ready(function () {
-    $('select').formSelect();
-});
-
-
-//note we are going to need this !advanced note: When dynamically changing the value of a textarea with methods like jQuery's
-// .val(), you must trigger an autoresize on it afterwords because .val() does not automatically trigger 
-// the events we've binded to the textarea.
-//$('#textarea1').val('New Text');
-//M.textareaAutoResize($('#textarea1'));
