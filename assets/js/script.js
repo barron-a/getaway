5c40f21fd7c88b8bf310b29f33f8ef5597f860f6
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2020-11-02 13:53:37 -0800

    input placed when enter key is pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,20 @@ var click = btn.onclick = function () {
 
 }
 
+// Get the input field
+var input = document.getElementById("zipcode");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function (event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("btn").click();
+    }
+});
+
 
 //drop down distance menu
 document.addEventListener('DOMContentLoaded', function () {
